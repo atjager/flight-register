4a7897d8d2146192767ff0dcb647539de9b79602
atjager <jageratti2000@gmail.com>
2024-04-09 14:41:16 +0200

    init

---
+++ src/main/java/com/aj/flightregister/FlightRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.aj.flightregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightRegisterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightRegisterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aj/flightregister/FlightRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aj.flightregister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightRegisterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
